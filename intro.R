76477cc13513dbde267b2222a3c47b7295125cf2
Georgiana Stan <stangeorgiana18@yahoo.com>
2024-01-31 08:33:57 +0000

    data str

--- intro.R
+++ intro.R
@@ -1,5 +1,28 @@
 print('First experience with RStudio')
 print('And I like it')
 
+# Ctrl + Enter - run highlighted line
+# Ctrl + Enter + Shift - execute all the code
+
+# bad style in R for var: bank_account
+# usually: bank.account
+
+# clear the console: Control + L
+
+# delete all variables: rm(list = ls())
+
+# assign logicals as TRUE/FALSE
+a <- T 
+
+print(a)
+
+# check data type class
+print(class(a))
+
+# numeric
+print(class(32.2)) 
+
+# character
+print(class("hello"))
 
 
