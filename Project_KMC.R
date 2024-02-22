3003bb06a6ded300ca7d9cbc5f4e0868ced2eb90
Georgiana Stan <stangeorgiana18@yahoo.com>
2024-02-22 22:52:44 +0000

    some addition

--- Project_KMC.R
+++ Project_KMC.R
@@ -3,11 +3,16 @@ df2 <- read.csv('winequality-white.csv', sep = ';')
 
 #print(head(df1))
 
-df1$label <- 'red'
-df2$label <- 'white'
+# apply lable to the df
+#df1$label <- 'red'
+#df2$label <- 'white'
 
+df1$label <- sapply(df1$pH, function(x){'red'}) # for every row in the pH column return red and assign that to the label column
+df2$label <- sapply(df2$pH, function(x){'white'}) 
 
 # bind the 2 df into 1
+#wine <- rbind(df1, df2) # or like that without dplyr lib
+
 library(dplyr)
 
 wine <- bind_rows(df1, df2) # by rows bcs they have the same columns 
@@ -17,7 +22,7 @@ wine <- bind_rows(df1, df2) # by rows bcs they have the same columns
 library(ggplot2)
 
 pl <- ggplot(wine, aes(residual.sugar)) + geom_histogram(aes(fill = label), color = 'black') + scale_fill_manual(values = c('red','white')) + theme_bw()
-print(pl)
+print(pl) # the red wine has a lower residual sugar count (also there are more white wine samples than red)
 
 
 pl <- ggplot(wine, aes(citric.acid)) + geom_histogram(aes(fill = label), color = 'black') + scale_fill_manual(values = c('red','white')) + theme_bw()
@@ -28,16 +33,17 @@ pl <- ggplot(wine, aes(alcohol)) + geom_histogram(aes(fill = label), color = 'bl
 print(pl)
 
 pl <- ggplot(wine, aes(citric.acid, residual.sugar)) + geom_point(aes(color = label), alpha = 0.5, size = 4) + scale_color_manual(values = c('red', 'white')) + theme_dark()
-print(pl)
+print(pl) # there's much noise/overlap
 
 pl <- ggplot(wine, aes(volatile.acidity, residual.sugar)) + geom_point(aes(color = label), alpha = 0.5, size = 4) + scale_color_manual(values = c('red', 'white')) + theme_dark()
 print(pl)
 
-clus.data <- select(wine, -label)
+clus.data <- select(wine, -label) 
+# or clus.data <- wine[, 1:12]
 #print(head(clus.data))
 
 wine.cluster <- kmeans(clus.data, 2, nstart = 20)
-print(wine.cluster)
+print(wine.cluster$centers) # the center value for each feature for each cluster
 
 # evaluate the clusters
 print(table(wine$label, wine.cluster$cluster))
